0a66d8459669d27d9e48a087c3e2491cb3130e24
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-25 23:16:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+  const baseUrl = 'http://localhost:3000/';
+  const configurationDogObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ name, email })
+  };
+
+  return fetch(`${baseUrl}users`, configurationDogObject)
+    .then(response => response.json())
+    .then(json => {
+      debugger
+      document.body.innerHTML = JSON.stringify(json["id"]);
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message;
+    })
+
+}
