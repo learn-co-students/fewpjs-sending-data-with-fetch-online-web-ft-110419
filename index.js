e1fef2d28f2265d8dbfc662fdc01f8c877e33474
Dominique Skaf <doskaf@yahoo.com>
2020-02-28 03:04:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+      body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let p = document.createElement("p");
+    p.innerHTML = object.id;
+    document.querySelector("body").appendChild(p);
+  })
+  .catch(function(error) {
+    let h = document.createElement("H1");
+    h.innerHTML = "Unauthorized Access";
+    document.querySelector("body").appendChild(h);
+    alert("Unauthorized Access");
+    console.log(error.message);
+  });
+}
