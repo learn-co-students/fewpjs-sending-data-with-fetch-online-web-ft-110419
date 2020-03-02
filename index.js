ff70aed0958a0dcdbb6b20bb98b5db65e56215e5
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-01 19:41:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const body = document.querySelector('body')
 
 function appendContentToBody(content) {
-    const output = content
-    const header = document.createElement('h1')
-    header.innerText = output
-    body.appendChild(header)
+
     return content
 }
 
@@ -31,10 +27,13 @@ function submitData(name, email) {
             return response.json()
         })
         .then(function(object) {
-            return appendContentToBody(object.id)
+            const body = document.querySelector('body')
+            const output = object.id
+            const header = document.createElement('h1')
+            header.innerText = output
+            body.appendChild(header)
         })
         .catch(function(error) {
-            appendContentToBody("Unauthorized Access")
             console.log(error.message)
         })
 }
