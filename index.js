e99dd2534fc9d38376e9f6787cf77c63101de59d
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-01 19:44:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-
-function appendContentToBody(content) {
-
-    return content
-}
-
-function submitData(name, email) {
-    const userName = name
-    const userEmail = email
-
-    let formData = {
-        name: userName,
-        email: userEmail
-    }
-
-    let configObj = {
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
+          "Content-Type": "application/json",
+          "Accept": "application/json"
         },
-        body: JSON.stringify(formData)
-    }
-
-    return fetch("http://localhost:3000/users", configObj)
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(object) {
-            const body = document.querySelector('body')
-            const output = object.id
-            const header = document.createElement('h1')
-            header.innerText = output
-            body.appendChild(header)
-        })
-        .catch(function(error) {
-            console.log(error.message)
-        })
-}
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+  
